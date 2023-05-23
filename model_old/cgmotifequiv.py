f99a74b65dbc95ee4e3476356ee58f5047a664a7
ajh011 <ajh011@lvc.edu>
2023-05-23 13:53:18 -0400

    add tested model architectures

---
+++ model_old/cgmotifequiv.py
@@ -0,0 +1,58 @@
+import copy
+import torch
+import numpy as np
+from torch_scatter import scatter
+from torch_geometric.nn.conv import HeteroConv, SAGEConv, TransformerConv, CGConv
+import torch_geometric.nn as nn
+from torch_geometric.nn import to_hetero
+
+
+class HeteroRelConv(torch.nn.Module):
+    def __init__(self, h_dim = 128, hout_dim = 256, n_layers = 3, orders = ['atom','bond','motif','cell'], a_dim= 92, b_dim=24, m_dim=35, c_dim=64):
+        super().__init__()
+        
+        self.orders = orders
+
+        self.lins = torch.nn.ModuleList()
+        self.convs = torch.nn.ModuleList() 
+        for i in range(n_layers):
+            conv = HeteroConv({
+#                ('motif','touches','motif'): CGConv(h_dim, batch_norm = True),
+                ('motif','contains','atom'): CGConv(h_dim, batch_norm=True),
+                ('motif','contains','bond'): CGConv(h_dim, batch_norm=True),
+#                ('bond','in','motif'): CGConv(h_dim, batch_norm=True),
+                ('bond','contains','atom'): CGConv(h_dim, batch_norm=True),
+                ('atom','bonds','atom'): CGConv(h_dim, batch_norm=True),
+#                ('atom','in','bond'): CGConv(h_dim, batch_norm=True),
+#                ('atom','in','motif'): CGConv(h_dim, batch_norm=True),
+#                ('atom','in','cell'): CGConv(h_dim, batch_norm=True),
+#                ('bond','in','cell'): CGConv(h_dim, batch_norm=True),
+#                ('motif','in','cell'): CGConv(h_dim, batch_norm=True),
+#                ('cell','contains','atom'): CGConv(h_dim, batch_norm=True),
+#                ('cell','contains','bond'): CGConv(h_dim , batch_norm=True),
+#                ('cell','contains','motif'): CGConv(h_dim , batch_norm=True), 
+                }, aggr='mean')
+            self.convs.append(conv)
+        for i in [a_dim,b_dim,m_dim]:
+            self.lins.append(nn.Linear(i,h_dim))
+        self.proj = nn.Linear(h_dim,hout_dim)
+        self.activation = torch.nn.Softplus()
+        self.out = nn.Linear(hout_dim,1)
+ 
+    def forward(self, x_dict, edge_index_dict, batch):
+        del x_dict['cell'] 
+        x_dict = {key: lin(x) for (key, x), lin in zip(x_dict.items(),self.lins)}
+        for conv in self.convs:
+            md = x_dict['motif']
+            x_dict = conv(x_dict,edge_index_dict)
+            x_dict = {key: x.relu() for key, x in x_dict.items()}
+            x_dict['motif'] = md
+        x = x_dict['atom']
+        x = scatter(x, batch, dim=0, reduce='mean') 
+        x = self.proj(x)
+        x = self.activation(x)
+        output = self.out(x)
+        return output
+
+
+       
