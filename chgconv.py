2cf4e5e2ccfab7232c488341cb185494723a3d60
ajh011 <46264051+ajh011@users.noreply.github.com>
2023-08-02 12:53:31 -0400

    Update chgconv.py

--- chgconv.py
+++ chgconv.py
@@ -1,103 +1,67 @@
-from typing import Tuple, Union
 
 import torch
 import torch.nn.functional as F
-from torch import Tensor
 from torch.nn import BatchNorm1d, Linear
-
 from torch_geometric.nn.conv import MessagePassing
-from torch_geometric.typing import Adj, OptTensor, PairTensor
+from torch_geometric.nn import aggr
 
 
-[docs]class CGConv(MessagePassing):
-    r"""The crystal graph convolutional operator from the
-    `"Crystal Graph Convolutional Neural Networks for an
-    Accurate and Interpretable Prediction of Material Properties"
-    <https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.120.145301>`_
-    paper
+class CHGConv(MessagePassing):
+    def __init__(self, node_fea_dim, hedge_fea_dim, hedge_agg_method = 'mean', node_agg_method = 'mean'):
+        super().__init__(aggr=agg_method)
+        self.node_fea_dim = node_fea_dim
+        self.hedge_fea_dim = hedge_fea_dim
 
-    .. math::
-        \mathbf{x}^{\prime}_i = \mathbf{x}_i + \sum_{j \in \mathcal{N}(i)}
-        \sigma \left( \mathbf{z}_{i,j} \mathbf{W}_f + \mathbf{b}_f \right)
-        \odot g \left( \mathbf{z}_{i,j} \mathbf{W}_s + \mathbf{b}_s  \right)
+        self.lin_f = Linear(2*node_fea_dim+hedge_fea_dim, node_fea_dim)
+        self.lin_c = Linear(2*node_fea_dim+hedge_fea_dim, node_fea_dim)
+        self.bn_f = BatchNorm1d(node_fea_dim)
+        self.bn_c = BatchNorm1d(node_fea_dim)
+        self.bn_o = BatchNorm1d(node_fea_dim)
 
-    where :math:`\mathbf{z}_{i,j} = [ \mathbf{x}_i, \mathbf{x}_j,
-    \mathbf{e}_{i,j} ]` denotes the concatenation of central node features,
-    neighboring node features and edge features.
-    In addition, :math:`\sigma` and :math:`g` denote the sigmoid and softplus
-    functions, respectively.
+        if hedge_agg_method == 'mean':
+            self.hedge_agg = aggr.MeanAggregation()
 
-    Args:
-        channels (int or tuple): Size of each input sample. A tuple
-            corresponds to the sizes of source and target dimensionalities.
-        dim (int, optional): Edge feature dimensionality. (default: :obj:`0`)
-        aggr (str, optional): The aggregation operator to use
-            (:obj:`"add"`, :obj:`"mean"`, :obj:`"max"`).
-            (default: :obj:`"add"`)
-        batch_norm (bool, optional): If set to :obj:`True`, will make use of
-            batch normalization. (default: :obj:`False`)
-        bias (bool, optional): If set to :obj:`False`, the layer will not learn
-            an additive bias. (default: :obj:`True`)
-        **kwargs (optional): Additional arguments of
-            :class:`torch_geometric.nn.conv.MessagePassing`.
+        if node_agg_method == 'mean':
+            self.node_agg = aggr.MeanAggregation()
 
-    Shapes:
-        - **input:**
-          node features :math:`(|\mathcal{V}|, F)` or
-          :math:`((|\mathcal{V_s}|, F_{s}), (|\mathcal{V_t}|, F_{t}))`
-          if bipartite,
-          edge indices :math:`(2, |\mathcal{E}|)`,
-          edge features :math:`(|\mathcal{E}|, D)` *(optional)*
-        - **output:** node features :math:`(|\mathcal{V}|, F)` or
-          :math:`(|\mathcal{V_t}|, F_{t})` if bipartite
-    """
-    def __init__(self, channels: Union[int, Tuple[int, int]], dim: int = 0,
-                 aggr: str = 'add', batch_norm: bool = False,
-                 bias: bool = True, **kwargs):
-        super().__init__(aggr=aggr, **kwargs)
-        self.channels = channels
-        self.dim = dim
-        self.batch_norm = batch_norm
+    def aggregate_hedge_xs(x, hedge_index):
+        xs = []
+        for i in hedge_index[0]:
+            xs.append(x[i])
 
-        if isinstance(channels, int):
-            channels = (channels, channels)
-
-        self.lin_f = Linear(sum(channels) + dim, channels[1], bias=bias)
-        self.lin_s = Linear(sum(channels) + dim, channels[1], bias=bias)
-        if batch_norm:
-            self.bn = BatchNorm1d(channels[1])
-        else:
-            self.bn = None
-
-        self.reset_parameters()
-
-[docs]    def reset_parameters(self):
-        super().reset_parameters()
-        self.lin_f.reset_parameters()
-        self.lin_s.reset_parameters()
-        if self.bn is not None:
-            self.bn.reset_parameters()
+        hedge_index_xs = torch.cat(xs, 0)
+        hedge_index_xs = self.hedge_agg(hedge_index_xs, index=hedge_index[1])
+        return hedge_index_xs
 
 
-[docs]    def forward(self, x: Union[Tensor, PairTensor], edge_index: Adj,
-                edge_attr: OptTensor = None) -> Tensor:
+    def forward(self, x, hedge_index, hedge_attr):
+        message_holder = []
+        hedge_index_xs = self.aggregate_hedge_xs(x, hedge_index)
+        for x_index, h_index in hedge_index:
+            z = torch.cat([x[x_index], hedge_attr[h_index], hedge_index_xs[h_index]], dim =-1)
+            z_f = self.lin_f(z)
+            z_f = self.bn_f(z_f)
+            z_c = self.lin_c(z)
+            z_c = self.bn_c(z_c)
+            message_holder.append(z_f.sigmoid() * F.softplus(z_c))
 
-        if isinstance(x, Tensor):
-            x: PairTensor = (x, x)
-
-        # propagate_type: (x: PairTensor, edge_attr: OptTensor)
-        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=None)
-        out = out if self.bn is None else self.bn(out)
-        out = out + x[1]
+        out = self.hedge_agg(message_holder, index=hedge_index[0])
+            
+        #out = self.propagate(hedge_index, x=x, hedge_attr=hedge_attr, hedge_index_xs=hedge_index_xs)
+        out = self.bn_o(out)
+        out = F.softplus(out + x)
         return out
+    
+         
 
+#    def message(self, x_i, hedge_attr, hedge_index_xs):
+#        z = torch.cat([x_i, x_j, hedge_attr], dim=-1)
 
-    def message(self, x_i, x_j, edge_attr: OptTensor) -> Tensor:
-        if edge_attr is None:
-            z = torch.cat([x_i, x_j], dim=-1)
-        else:
-            z = torch.cat([x_i, x_j, edge_attr], dim=-1)
-        return self.lin_f(z).sigmoid() * F.softplus(self.lin_s(z))
+#        z_f = self.lin_f(z)
+#        z_f = self.bn_f(z_f)
 
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}({self.channels}, dim={self.dim})'
+#        z_c = self.lin_c(z)
+#        z_c = self.bn_c(z_c)
+
+#        return z_f.sigmoid() * F.softplus(z_c)
+
